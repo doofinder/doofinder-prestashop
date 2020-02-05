8f14482c2e75b0b9ad930df6e8d44e2cfcb78ca5
danidomen <danimdomen11@gmail.com>
2020-02-05 17:26:49 +0100

    v3.1.2
    - Add checks for Doofinder error (for example, 429 query limit reached )& callback to native search
    - Add check to not load custom.css if you don't have any on custom css tab to avoid load unnecessary files on front

--- lib/SearchController.php
+++ lib/SearchController.php
@@ -18,79 +18,83 @@ class SearchController extends SearchControllerCore
 
     public function initContent()
     {
+        $parentContent = true;
         if (Module::isEnabled('doofinder')){
             $m = Module::getInstanceByName('doofinder');
-            $olderThan17 = (version_compare(_PS_VERSION_, '1.7', '<') === true);
-            $overwrite_embedded = Configuration::get('DF_OWSEARCHEB', null);
-            if ($overwrite_embedded) {
-                //HERE you are using embedded layer
-                if  ($olderThan17) {
-                    $this->setTemplate($m->getEmbeddedTemplateLocation());
-                    FrontController::initContent();
-                } else {
-                    ProductListingFrontController::initContent();
-                    $html = $this->context->smarty->fetch($m->getEmbeddedTemplateLocation());
-                    $this->context->smarty->assign(array(
-                        'HOOK_HOME' => trim($html),
-                    ));
-                    $this->setTemplate('index');
-                }
-            } elseif ($olderThan17) {
-                //HERE you are using API!
-                //If your PS is 1.6 or older. Newer are using productSearchProvider hook
-                $query = Tools::getValue('search_query', Tools::getValue('ref', Tools::getValue('s')));
-                $overwrite_search = Configuration::get('DF_OWSEARCH', null);           
-                $this->p = abs((int) (Tools::getValue('p', 1)));
-                $this->n = abs((int) (Tools::getValue('n', Configuration::get('PS_PRODUCTS_PER_PAGE'))));
-                
-                if ($overwrite_search && ($search = $m->generateSearch(true)) && $query && !is_array($query)) {
-                    $original_query = $query;
-                    $query = Tools::replaceAccentedChars(urldecode($query));
-
-                    foreach ($search['result'] as &$product) {
-                        $product['link'] .= (strpos($product['link'], '?') === false ? '?' : '&') . 'search_query=' . urlencode($query) . '&results=' . (int) $search['total'];
-                    }
-                    Hook::exec('actionSearch', array('expr' => $query, 'total' => $search['total']));
-                    $nbProducts = $search['total'];
-                    $this->pagination($nbProducts);
-
-                    if (method_exists($this, 'addColorsToProductList')) { 
-                        //RETROCOMPATIBILITY
-                        $this->addColorsToProductList($search['result']);
-                    }
-
-                    if (method_exists('ImageType', 'getFormatedName')) {
-                        $imageSize = Image::getSize(ImageType::getFormatedName('home'));
+            if ($m->testDoofinderApi(Context::getContext()->language->iso_code)) {
+                $olderThan17 = (version_compare(_PS_VERSION_, '1.7', '<') === true);
+                $overwrite_embedded = Configuration::get('DF_OWSEARCHEB', null);
+                if ($overwrite_embedded) {
+                    //HERE you are using embedded layer
+                    if  ($olderThan17) {
+                        $parentContent = false;
+                        $this->setTemplate($m->getEmbeddedTemplateLocation());
+                        FrontController::initContent();
                     } else {
-                        $imageSize = Image::getSize($m->getFormatedName('home'));
+                        $parentContent = false;
+                        ProductListingFrontController::initContent();
+                        $html = $this->context->smarty->fetch($m->getEmbeddedTemplateLocation());
+                        $this->context->smarty->assign(array(
+                            'HOOK_HOME' => trim($html),
+                        ));
+                        $this->setTemplate('index');
                     }
+                } elseif ($olderThan17) {
+                    //HERE you are using API!
+                    //If your PS is 1.6 or older. Newer are using productSearchProvider hook
+                    $query = Tools::getValue('search_query', Tools::getValue('ref', Tools::getValue('s')));
+                    $overwrite_search = Configuration::get('DF_OWSEARCH', null);           
+                    $this->p = abs((int) (Tools::getValue('p', 1)));
+                    $this->n = abs((int) (Tools::getValue('n', Configuration::get('PS_PRODUCTS_PER_PAGE'))));
+                    
+                    if ($overwrite_search && ($search = $m->generateSearch(true)) && $query && !is_array($query)) {
+                        $parentContent = false;
+                        $original_query = $query;
+                        $query = Tools::replaceAccentedChars(urldecode($query));
 
-                    $this->context->smarty->assign(
-                        array(
-                            'products' => $search['result'], // DEPRECATED (since to 1.4), not use this: conflict with block_cart module
-                            'search_products' => $search['result'],
-                            'nbProducts' => $search['total'],
-                            'search_query' => $original_query,
-                            'homeSize' => $imageSize
-                        )
-                    );
+                        foreach ($search['result'] as &$product) {
+                            $product['link'] .= (strpos($product['link'], '?') === false ? '?' : '&') . 'search_query=' . urlencode($query) . '&results=' . (int) $search['total'];
+                        }
+                        Hook::exec('actionSearch', array('expr' => $query, 'total' => $search['total']));
+                        $nbProducts = $search['total'];
+                        $this->pagination($nbProducts);
 
-                    $this->context->smarty->assign(
-                        array(
-                            'add_prod_display' => Configuration::get('PS_ATTRIBUTE_CATEGORY_DISPLAY'), 
-                            'comparator_max_item' => Configuration::get('PS_COMPARATOR_MAX_ITEM')
-                        )
-                    );
+                        if (method_exists($this, 'addColorsToProductList')) { 
+                            //RETROCOMPATIBILITY
+                            $this->addColorsToProductList($search['result']);
+                        }
 
-                    $this->setTemplate(_PS_THEME_DIR_ . 'search.tpl');
-                    FrontController::initContent();
-                } else {
-                    parent::initContent();
+                        if (method_exists('ImageType', 'getFormatedName')) {
+                            $imageSize = Image::getSize(ImageType::getFormatedName('home'));
+                        } else {
+                            $imageSize = Image::getSize($m->getFormatedName('home'));
+                        }
+
+                        $this->context->smarty->assign(
+                            array(
+                                'products' => $search['result'], // DEPRECATED (since to 1.4), not use this: conflict with block_cart module
+                                'search_products' => $search['result'],
+                                'nbProducts' => $search['total'],
+                                'search_query' => $original_query,
+                                'homeSize' => $imageSize
+                            )
+                        );
+
+                        $this->context->smarty->assign(
+                            array(
+                                'add_prod_display' => Configuration::get('PS_ATTRIBUTE_CATEGORY_DISPLAY'), 
+                                'comparator_max_item' => Configuration::get('PS_COMPARATOR_MAX_ITEM')
+                            )
+                        );
+
+                        $this->setTemplate(_PS_THEME_DIR_ . 'search.tpl');
+                        FrontController::initContent();
+                    }
                 }
-            } else {
-                parent::initContent();
             }
-        } else {
+        }
+
+        if ($parentContent) {
             parent::initContent();
         }
     }
