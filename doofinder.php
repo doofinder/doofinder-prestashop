f61eecdf41796e3f028a982dffac9538a9f8c015
Iván Gallego <igallego@webimpacto.es>
2021-10-25 18:29:53 +0200

    v4.0.3 Delete indices, inactive and search_url at the request to create engine

--- doofinder.php
+++ doofinder.php
@@ -45,7 +45,7 @@ class Doofinder extends Module
 
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.0.2';
+    const VERSION = '4.0.3';
     const YES = 1;
     const NO = 0;
 
@@ -53,7 +53,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.0.2';
+        $this->version = '4.0.3';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.7');
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
@@ -2287,12 +2287,9 @@ class Doofinder extends Module
                     $shop_name = $this->getShopURL($shopId).' | Lang:'.$liso.' Currency:'.$ciso;
 
                     $seRequest = '{
-                        "inactive": false,
-                        "indices": [],
                         "language": "'.$liso.'",
                         "currency": "'.$ciso.'",
                         "name": "'.$shop_name.'",
-                        "search_url": "http://'.$region.'-search.doofinder.com",
                         "site_url": "'.$this->getShopURL($shopId).'",
                         "stopwords": false
                     }';
