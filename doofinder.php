bd81be1fa3f42150104f4d38b1a5630eec359e7c
Abraham <abraham@doofinder.com>
2022-01-31 15:28:24 +0100

    add new version for translation of atribute names in url

--- doofinder.php
+++ doofinder.php
@@ -45,7 +45,7 @@ class Doofinder extends Module
 
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.0.5';
+    const VERSION = '4.0.6';
     const YES = 1;
     const NO = 0;
 
@@ -53,7 +53,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.0.5';
+        $this->version = '4.0.6';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.7');
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
