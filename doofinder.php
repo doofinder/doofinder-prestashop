7b95700edc69f6be39d369be9b701d1ddae9c4ba
Abraham <abraham@doofinder.com>
2022-08-17 10:06:09 +0200

    category-path-not-saved-correctly

--- doofinder.php
+++ doofinder.php
@@ -46,7 +46,7 @@ class Doofinder extends Module
 
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.3.1';
+    const VERSION = '4.3.2';
     const YES = 1;
     const NO = 0;
 
@@ -54,7 +54,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.3.1';
+        $this->version = '4.3.2';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.7');
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
@@ -1221,7 +1221,7 @@ class Doofinder extends Module
                 $value = implode(',', $value);
             }
             if ($postKey === 'DF_FEED_FULL_PATH') {
-                Configuration::updateValue('DF_FEED_MAINCATEGORY_PATH', !$value);
+                Configuration::updateValue('DF_FEED_MAINCATEGORY_PATH', 0);
             }
             $value = trim($value);
             $html = false;
