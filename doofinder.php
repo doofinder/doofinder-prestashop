055b7066ec8a03da77593a753bf1958bdba07be2
Pedro <pedro@doofinder.com>
2023-01-16 15:11:32 +0000

    Bump up version

--- doofinder.php
+++ doofinder.php
@@ -44,7 +44,7 @@ class Doofinder extends Module
 
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.3.15';
+    const VERSION = '4.3.16';
     const YES = 1;
     const NO = 0;
 
@@ -52,7 +52,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.3.15';
+        $this->version = '4.3.16';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => '1.7'];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
