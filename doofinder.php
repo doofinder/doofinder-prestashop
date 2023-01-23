43bdf0a21c4c53e84b079f358a3264fe3f9de455
Pedro Cruz <pedro@doofinder.com>
2023-01-23 11:18:53 +0000

    Changes in buildFeedUrl (#108)

--- doofinder.php
+++ doofinder.php
@@ -44,7 +44,7 @@ class Doofinder extends Module
 
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.3.16';
+    const VERSION = '4.3.17';
     const YES = 1;
     const NO = 0;
 
@@ -52,7 +52,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.3.16';
+        $this->version = '4.3.17';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => '1.7'];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
@@ -261,6 +261,7 @@ class Doofinder extends Module
         }
         $this->migrateOldConfigHashIDs();
         $adv = Tools::getValue('adv', 0);
+
         $this->context->smarty->assign('adv', $adv);
 
         $msg = $this->postProcess();
@@ -360,8 +361,8 @@ class Doofinder extends Module
     {
         $shop_url = $this->getShopURL($shop_id);
 
-        return $shop_url . ltrim($this->_path, DIRECTORY_SEPARATOR)
-            . 'feed.php?'
+        return $shop_url . ltrim('modules/' . $this->name, DIRECTORY_SEPARATOR)
+            . '/feed.php?'
             . 'currency=' . Tools::strtoupper($currency)
             . '&language=' . Tools::strtoupper($language)
             . '&dfsec_hash=' . Configuration::get('DF_API_KEY');
