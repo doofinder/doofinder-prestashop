aa8db7094e7e20319f3d28f0f1de7c076bc36d7a
Pedro <pedro@doofinder.com>
2023-02-03 15:16:32 +0000

    Changes in build script to remove some files from zip

--- build.sh
+++ build.sh
@@ -4,4 +4,21 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-git archive --format=zip -o doofinder-$1.zip --prefix doofinder/ $1
+cd ..
+
+mkdir _build
+cp -R doofinder-prestashop _build
+mv _build/doofinder-prestashop _build/doofinder
+
+cd _build
+
+rm doofinder/docker-compose.yml
+rm doofinder/composer.json
+rm -rf doofinder/.git
+rm -rf doofinder/.github
+rm doofinder/.git*
+
+zip -r doofinder-$1.zip doofinder
+mv doofinder-$1.zip ..
+cd ..
+rm -rf _build
