c59c54ed86c82e2d50ce190236d03f9a311ed284
Pedro <pedro@doofinder.com>
2023-02-06 10:03:18 +0000

    Removed some unwanted files from the zip file

--- build.sh
+++ build.sh
@@ -4,21 +4,4 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-cd ..
-
-mkdir _build
-cp -R doofinder-prestashop _build
-mv _build/doofinder-prestashop _build/doofinder
-
-cd _build
-
-rm doofinder/docker-compose.yml
-rm doofinder/composer.json
-rm -rf doofinder/.git
-rm -rf doofinder/.github
-rm doofinder/.git*
-
-zip -r doofinder-$1.zip doofinder
-mv doofinder-$1.zip ..
-cd ..
-rm -rf _build
+git archive --format=zip -o doofinder-$1.zip --prefix doofinder/ $1
