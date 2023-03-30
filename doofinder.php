78b95e257507dd1a9c3f16540748b12194c11d83
Pedro Rivas <pedro.rivas@doofinder.com>
2023-03-30 12:11:56 +0200

    Fix format

--- doofinder.php
+++ doofinder.php
@@ -31,17 +31,17 @@ class Doofinder extends Module
     public $ps_layered_full_tree = true;
     public $searchBanner = false;
 
-    public const GS_SHORT_DESCRIPTION = 1;
-    public const GS_LONG_DESCRIPTION = 2;
-    public const VERSION = '4.4.5';
-    public const YES = 1;
-    public const NO = 0;
+    const GS_SHORT_DESCRIPTION = 1;
+    const GS_LONG_DESCRIPTION = 2;
+    const VERSION = '4.4.6';
+    const YES = 1;
+    const NO = 0;
 
     public function __construct()
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.4.5';
+        $this->version = '4.4.6';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => _PS_VERSION_];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
