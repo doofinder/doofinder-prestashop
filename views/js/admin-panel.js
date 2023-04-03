164121f8b55e1d193fb7b6d4f370d131c68997a8
Pedro Rivas <pedro.rivas@doofinder.com>
2023-04-03 14:04:22 +0200

    Fix license

--- views/js/admin-panel.js
+++ views/js/admin-panel.js
@@ -1,3 +1,17 @@
+/**
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ * @author    Doofinder
+ * @copyright Doofinder
+ * @license   GPLv3
+ */
+
 $(document).ready(function() {
 
   addVisibilityDependency('DF_GS_DISPLAY_PRICES', 'DF_GS_PRICES_USE_TAX_on');
