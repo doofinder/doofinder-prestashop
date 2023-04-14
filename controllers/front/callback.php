b7816c883ecc3ae507fe0607041db1ed807333f2
Pedro Rivas <pedro.rivas@doofinder.com>
2023-04-14 13:13:59 +0200

    Add doofinder.com as allowed origin

--- controllers/front/callback.php
+++ controllers/front/callback.php
@@ -6,10 +6,7 @@ class DoofinderCallbackModuleFrontController extends ModuleFrontController
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header('Content-Type: application/json');
-            header('Access-Control-Allow-Origin: *');
-            header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
-            header('Access-Control-Allow-Headers: x-requested-with, Content-Type, origin, authorization, accept, client-security-token');
-
+            header('Access-Control-Allow-Origin: https://*.doofinder.com');
             Configuration::updateValue('DF_FEED_INDEXED', true);
 
             exit(json_encode(['status' => 'success']));
