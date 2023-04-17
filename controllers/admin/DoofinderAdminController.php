65c4240d126bfa4193b09110809435df9637a6f5
Pedro Rivas <pedro.rivas@doofinder.com>
2023-04-17 13:31:54 +0200

    Fix validator issues

--- controllers/admin/DoofinderAdminController.php
+++ controllers/admin/DoofinderAdminController.php
@@ -1,16 +1,3 @@
-/**
- * NOTICE OF LICENSE
- *
- * This file is licenced under the Software License Agreement.
- * With the purchase or the installation of the software in your application
- * you accept the licence agreement.
- *
- * You must not modify, adapt or create derivative works of this source code
- *
- * @author    Doofinder
- * @copyright Doofinder
- * @license   GPLv3
- */
 <?php
 /**
  * NOTICE OF LICENSE
