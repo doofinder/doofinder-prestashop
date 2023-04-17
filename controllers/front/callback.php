ccac73f35b2a95c6941d06c2922da26ec8dd26fc
Pedro Rivas <rmdrobs@gmail.com>
2023-04-17 07:24:55 -0400

    Pedror/unplugged 309 not activate layer until indexing completed (#126)
    * Fix plugin validator suggestions

--- controllers/front/callback.php
+++ controllers/front/callback.php
@@ -1,3 +1,16 @@
+/**
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ * @author    Doofinder
+ * @copyright Doofinder
+ * @license   GPLv3
+ */
 <?php
 
 class DoofinderCallbackModuleFrontController extends ModuleFrontController
