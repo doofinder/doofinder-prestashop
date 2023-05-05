c625c296dc902c144d3875e3c678447bfe2edc65
Iván <ivan.gallego@doofinder.com>
2023-05-05 10:09:18 +0200

    Endpoint to clear the landing cache

---
+++ cache.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ * @author    Doofinder
+ * @copyright Doofinder
+ * @license   GPLv3
+ */
+require_once dirname(__FILE__) . '/../../config/config.inc.php';
+
+if (Tools::isSubmit('landing')) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        return Db::getInstance()->delete('doofinder_landing');
+    }
+    exit;
+}
