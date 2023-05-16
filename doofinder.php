d1e49a8eccdbcade320d1e49335ff5cee71795ed
Iván <ivan.gallego@doofinder.com>
2023-05-16 09:27:49 +0200

    replace landings cache encoding

--- doofinder.php
+++ doofinder.php
@@ -33,7 +33,7 @@ class Doofinder extends Module
 
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.6.1';
+    const VERSION = '4.6.2';
     const YES = 1;
     const NO = 0;
 
@@ -41,7 +41,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.6.1';
+        $this->version = '4.6.2';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => _PS_VERSION_];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
