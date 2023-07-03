3edad3a4e81e8fa1d30968c43b803330f9dea6ec
davidmolinacano <davidmolinacano@gmail.com>
2023-07-03 12:22:22 +0200

    Restored initial screen error message

--- doofinder.php
+++ doofinder.php
@@ -31,6 +31,8 @@ class Doofinder extends Module
     public $ps_layered_full_tree = true;
     public $searchBanner = false;
 
+    // Feel free to change this value to your own local env or ngrok 
+    const DOOMANAGER_URL = 'https://admin.doofinder.com/';
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
     const VERSION = '4.6.4';
@@ -277,6 +279,7 @@ class Doofinder extends Module
         $this->context->smarty->assign('configured', $configured);
         $this->context->smarty->assign('is_new_shop', $is_new_shop);
         $this->context->smarty->assign('shop_id', $shop_id);
+        $this->context->smarty->assign('checkConnection', $this->checkOutsideConnection());
         $this->context->smarty->assign('tokenAjax', Tools::encrypt('doofinder-ajax'));
         $this->context->smarty->assign('skipurl', $skipurl);
         $this->context->smarty->assign('paramsPopup', $paramsPopup);
@@ -1548,6 +1551,27 @@ class Doofinder extends Module
         }
 
         return true;
+    }
+
+    /**
+     * Checks the connection with DooManager
+     *
+     * @return bool
+     */
+    public function checkOutsideConnection()
+    {
+        // Require only on this function to not overload memory with not needed classes
+        require_once _PS_MODULE_DIR_ . 'doofinder/lib/EasyREST.php';
+        $client = new EasyREST(true, 3);
+        $result = $client->get(sprintf('%sauth/login', DOOMANAGER_URL));
+        if (
+            $result && $result->originalResponse && isset($result->headers['code'])
+            && (strpos($result->originalResponse, 'HTTP/2 200') || $result->headers['code'] == 200)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
