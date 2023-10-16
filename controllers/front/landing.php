f62e3fc06087ad011d76226c961751dbb9138291
vicfebo@gmail.com <vicfebo@gmail.com>
2023-10-16 15:24:43 +0200

    head condition

--- controllers/front/landing.php
+++ controllers/front/landing.php
@@ -16,6 +16,7 @@ use PrestaShop\PrestaShop\Adapter\Image\ImageRetriever;
 use PrestaShop\PrestaShop\Adapter\Product\PriceFormatter;
 use PrestaShop\PrestaShop\Adapter\Product\ProductColorsRetriever;
 use PrestaShop\PrestaShop\Core\Product\ProductListingPresenter;
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
