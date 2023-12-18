fc3523978c717751e898e318467f94d15c5c6051
Iván <ivan.gallego@doofinder.com>
2023-12-18 15:10:43 +0100

    Check for set_time_limit function when generating feed

--- feeds/product.php
+++ feeds/product.php
@@ -23,7 +23,9 @@
  * - taxes:      Boolean. Apply taxes to prices. Default true.
  * - prices:     Boolean. Display Prices. Default true.
  */
-@set_time_limit(3600 * 2);
+if (function_exists('set_time_limit')) {
+    @set_time_limit(3600 * 2);
+}
 
 require_once dirname(__FILE__) . '/../../../config/config.inc.php';
 require_once dirname(__FILE__) . '/../../../init.php';
