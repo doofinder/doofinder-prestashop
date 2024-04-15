d2e6f3533e2918f7fc0dc4c2ade9cb6a740a6d7c
Juan García <srgarci79@gmail.com>
2024-04-15 12:11:48 +0200

    pass quantity when adding products to cart

--- views/js/add-to-cart/doofinder-add_to_cart_ps16.js
+++ views/js/add-to-cart/doofinder-add_to_cart_ps16.js
@@ -15,5 +15,5 @@
 //implementation of "add to cart" functionality for prestashop 1.6.x
 
 let doofinderManageCart = (cartOptions) => {
-    ajaxCart.add(cartOptions.productID, cartOptions.customizationID);
+    ajaxCart.add(cartOptions.productID, cartOptions.customizationID, undefined, undefined, cartOptions.quantity);
 }
