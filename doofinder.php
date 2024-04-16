caa55888e468408f553c5ab994f7d941362cb580
eduardogomez97 <ryukedu86@gmail.com>
2024-04-16 17:36:11 +0200

    Fix tag issue in landing pages

--- doofinder.php
+++ doofinder.php
@@ -32,7 +32,7 @@ class Doofinder extends Module
     const DOOMANAGER_URL = 'https://admin.doofinder.com';
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.7.27';
+    const VERSION = '4.7.28';
     const YES = 1;
     const NO = 0;
 
@@ -40,7 +40,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.7.27';
+        $this->version = '4.7.28';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => _PS_VERSION_];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
@@ -1402,7 +1402,7 @@ class Doofinder extends Module
             $product_pool_attributes = [];
             $product_pool_ids = [];
             foreach ($dfResultsArray as $entry) {
-                if ($entry['type'] == 'product') {
+                if (in_array($entry['type'], array("product", "products"))) {
                     if (strpos($entry['id'], 'VAR-') === false) {
                         $product_pool_ids[] = (int) pSQL($entry['id']);
                     } else {
