9798f6ad5e0ab61edfa340610c9a2157e453b425
Yunier López Martinez <yunier0525+yuloma@gmail.com>
2024-09-30 16:38:28 +0200

    increase version

--- doofinder.php
+++ doofinder.php
@@ -33,7 +33,7 @@ class Doofinder extends Module
     const DOOMANAGER_URL = 'https://admin.doofinder.com';
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.8.8';
+    const VERSION = '4.8.9';
     const YES = 1;
     const NO = 0;
 
@@ -41,7 +41,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.8.8';
+        $this->version = '4.8.9';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => _PS_VERSION_];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
