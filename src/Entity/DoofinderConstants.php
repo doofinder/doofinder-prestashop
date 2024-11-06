17a41bdcfc94d364a691b6d8ac659d62c4c0e213
Yunier López Martinez <110968332+yuloma@users.noreply.github.com>
2024-11-06 11:06:34 +0100

    Fix missing name (#256)
    * fix missing name attribute
    
    * increase version
    
    * readonly inputs on adv=0 and scroll

--- src/Entity/DoofinderConstants.php
+++ src/Entity/DoofinderConstants.php
@@ -27,7 +27,7 @@ class DoofinderConstants
     const DOOPHOENIX_REGION_URL = 'https://%ssearch.doofinder.com';
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
-    const VERSION = '4.9.4';
+    const VERSION = '4.9.5';
     const NAME = 'doofinder';
     const YES = 1;
     const NO = 0;
