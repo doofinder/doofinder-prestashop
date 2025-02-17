2781d4f08c2b84bd92140b137c2b06bebff3dc09
davidmolinacano <davidmolinacano@gmail.com>
2025-02-17 07:51:21 +0100

    version bump to 4.12.6

--- doofinder.php
+++ doofinder.php
@@ -39,7 +39,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '4.12.5';
+        $this->version = '4.12.6';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => _PS_VERSION_];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
