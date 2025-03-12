4f4b91df0c549ab836b688ca3c77996369fffd3f
Alberto Mur <alberto.mur@doofinder.com>
2025-03-12 15:21:38 +0100

    Add `.php-cs-fixer.dist.php`

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$prestashop_config = new PrestaShop\CodingStandards\CsFixer\Config();
+$prestashop_rules = $prestashop_config->getRules();
+$prestashop_rules['blank_line_after_opening_tag'] = false;
+
+$config = new PhpCsFixer\Config('PrestaShop coding standard');
+$config->setRiskyAllowed(true)
+    ->setRules($prestashop_rules);
+
+/** @var Symfony\Component\Finder\Finder $finder */
+$finder = $config->setUsingCache(true)->getFinder();
+$finder->in(__DIR__)->exclude('vendor')->exclude('html');
+
+return $config;
