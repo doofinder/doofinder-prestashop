fae8d48daafae8fd10fafcef382218739c006388
Alberto Mur <alberto.mur@doofinder.com>
2025-06-13 13:18:23 +0200

    Create new database connections to allow multiple unbuffered queries

--- src/Entity/DfTools.php
+++ src/Entity/DfTools.php
@@ -32,6 +32,14 @@ class DfTools
     protected static $rootCategoryIds;
     protected static $cachedCategoryPaths = [];
 
+    /** @var array List of server settings */
+    public static $_servers = [];
+
+    /** @var null Flag used to load slave servers only once.
+     * See loadSlaveServers() method
+     */
+    public static $_slave_servers_loaded = null;
+
     /**
      * Hash password.
      *
@@ -492,6 +500,74 @@ class DfTools
     }
 
 
+    /**
+     * Creates a new database instance.
+     *
+     * This method initializes and returns a new database connection instance, either to the master server
+     * or to a slave server based on the parameter. It manages connection pooling and sets up
+     * unbuffered queries for PDO connections to handle large datasets efficiently.
+     *
+     * @param bool $master Whether to connect to the master server (true) or a slave server (false)
+     * @return \Db Database instance with active connection
+     */
+    protected static function getNewDbInstance($master = true)
+    {
+        static $id = 0;
+
+        // This MUST not be declared with the class members because some defines (like _DB_SERVER_) may not exist yet (the constructor can be called directly with params)
+        if (!self::$_servers) {
+            self::$_servers = [
+                ['server' => _DB_SERVER_, 'user' => _DB_USER_, 'password' => _DB_PASSWD_, 'database' => _DB_NAME_], /* MySQL Master server */
+            ];
+        }
+
+        if (!$master) {
+            self::loadSlaveServers();
+        }
+
+        $total_servers = count(self::$_servers);
+        if ($master || $total_servers == 1) {
+            $id_server = 0;
+        } else {
+            ++$id;
+            $id_server = ($total_servers > 2 && ($id % $total_servers) != 0) ? $id % $total_servers : 1;
+        }
+
+        $class = \Db::getClass();
+        $instance = new $class(
+            self::$_servers[$id_server]['server'],
+            self::$_servers[$id_server]['user'],
+            self::$_servers[$id_server]['password'],
+            self::$_servers[$id_server]['database'],
+            false
+        );
+        $link = $instance->connect();
+        if ( 'DbPDO' === $class && method_exists($link, 'setAttribute')) {
+            // Set the PDO attribute to not use buffered queries
+            // This is needed to avoid memory issues with large datasets
+
+            $link->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+        }
+        return $instance;
+    }
+
+     /**
+     * Loads configuration settings for slave servers if needed.
+     */
+    protected static function loadSlaveServers()
+    {
+        if (self::$_slave_servers_loaded !== null) {
+            return;
+        }
+
+        // Add here your slave(s) server(s) in this file
+        if (file_exists(_PS_ROOT_DIR_ . '/config/db_slave_server.inc.php')) {
+            self::$_servers = array_merge(self::$_servers, require (_PS_ROOT_DIR_ . '/config/db_slave_server.inc.php'));
+        }
+
+        self::$_slave_servers_loaded = true;
+    }
+
     public static function getAvailableProductsForLanguageV2($idLang, $checkLeadership=true, $limit = false, $offset = false, $ids = null)
     {
 
@@ -504,7 +580,7 @@ class DfTools
         }
 
         // Product table fields
-        $query->select('p.ean13 AS ean13, p.upc, p.reference');
+        $query->select('p.ean13 AS ean13, p.upc, p.reference, p.supplier_reference');
 
         // Product shop table fields
         $query->select('product_shop.id_product, product_shop.show_price, product_shop.available_for_order');
@@ -644,7 +720,7 @@ class DfTools
         // echo "<pre>";
         // var_dump($query->__toString());
         // echo "</pre>";
-        return \Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($query, false, false);
+        return self::getNewDbInstance(_PS_USE_SQL_SLAVE_)->executeS($query, false, false);
     }
 
     public static function getProductVariationsV2($idProduct, $idLang) {
@@ -704,7 +780,7 @@ class DfTools
 
         $query->groupBy('product_attribute_shop.id_product, product_attribute_shop.id_product_attribute');
 
-        return \Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($query, false, false);
+        return self::getNewDbInstance(_PS_USE_SQL_SLAVE_)->executeS($query, false, false);
     }
 
 
