33cea5e0c253ec36586cb08f42c88cf4027ec180
David Molina Cano <128705267+davidmolinacano@users.noreply.github.com>
2025-06-30 09:24:40 +0200

    feat: Simplified Integrations logic, moved almost all the parts to the single script (#349)

--- doofinder.php
+++ doofinder.php
@@ -39,7 +39,7 @@ class Doofinder extends Module
     {
         $this->name = 'doofinder';
         $this->tab = 'search_filter';
-        $this->version = '5.1.16';
+        $this->version = '5.1.17';
         $this->author = 'Doofinder (http://www.doofinder.com)';
         $this->ps_versions_compliancy = ['min' => '1.5', 'max' => '9.0.0'];
         $this->module_key = 'd1504fe6432199c7f56829be4bd16347';
