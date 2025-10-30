e11364a5ae36232d184a64727c27cd3d09ff6acc
Alberto Mur <alberto.mur@doofinder.com>
2025-10-30 17:40:33 +0100

    Update region url

--- src/Core/DoofinderConstants.php
+++ src/Core/DoofinderConstants.php
@@ -36,10 +36,10 @@ if (!defined('_PS_VERSION_')) {
 
 class DoofinderConstants
 {
-    const DOOMANAGER_REGION_URL = 'https://mursisoy-doomanager.ngrok.doofinder.com';
-    const DOOPLUGINS_REGION_URL = 'https://mursisoy-dooplugins.ngrok.doofinder.com';
-    const DOOPHOENIX_REGION_URL = 'https://mursisoy-doophoenix.ngrok.doofinder.com';
-    const CONFIG_REGION_URL = 'https://mursisoy-fakes3.ngrok.doofinder.com/eu1-df-store-config-files';
+    const DOOMANAGER_REGION_URL = 'https://%sadmin.doofinder.com';
+    const DOOPLUGINS_REGION_URL = 'https://%splugins.doofinder.com';
+    const DOOPHOENIX_REGION_URL = 'https://%ssearch.doofinder.com';
+    const CONFIG_REGION_URL = 'https://%s-config.doofinder.com';
     const GS_SHORT_DESCRIPTION = 1;
     const GS_LONG_DESCRIPTION = 2;
     const VERSION = '6.2.2';
