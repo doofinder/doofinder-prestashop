96aea95bb17f4083a180716696de824372088715
davidmolinacano <davidmolinacano@gmail.com>
2026-01-28 15:17:29 +0100

    fix: Bring the correct wholesale_price from DB

--- src/Utils/DfTools.php
+++ src/Utils/DfTools.php
@@ -510,7 +510,7 @@ class DfTools
         }
 
         // Product table fields
-        $query->select('p.ean13 AS ean13, p.upc, p.reference, p.wholesale_price');
+        $query->select('p.ean13 AS ean13, p.upc, p.reference, product_shop.wholesale_price');
 
         // Product shop table fields
         $query->select('product_shop.id_product, product_shop.show_price, product_shop.available_for_order, product_shop.minimal_quantity AS minimum_quantity, product_shop.date_add AS creation_date');
