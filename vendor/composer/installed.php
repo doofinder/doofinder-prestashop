003336441a667c1022d025eaf5cd144d9ac7641f
davidmolinacano <davidmolinacano@gmail.com>
2026-04-06 13:29:21 +0200

    feat: Use again item_id for add to cart

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '8.0.8',
-        'version' => '8.0.8.0',
+        'pretty_version' => '8.0.9',
+        'version' => '8.0.9.0',
         'type' => 'prestashop-module',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'prestashop/doofinder' => array(
-            'pretty_version' => '8.0.8',
-            'version' => '8.0.8.0',
+            'pretty_version' => '8.0.9',
+            'version' => '8.0.9.0',
             'type' => 'prestashop-module',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
