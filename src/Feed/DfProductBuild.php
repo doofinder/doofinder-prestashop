0dccb32c323ec6335abe4801075e5cad92741a49
David Molina Cano <128705267+davidmolinacano@users.noreply.github.com>
2026-08-25 10:28:00 +0200

    PLUGINRANGERS-5296 | Delegate some fields sanitization to the backend (#453)
    * feat: Delegate desc and alt_desc sanitization to another service
    
    * feat: Stop sanitizing also title, meta_desc and meta_title

--- src/Feed/DfProductBuild.php
+++ src/Feed/DfProductBuild.php
@@ -1032,12 +1032,12 @@ class DfProductBuild
         if ($this->productVariations) {
             $p['item_group_id'] = $this->getItemGroupId($product);
         }
-        $p['title'] = DfTools::cleanString($product['name']);
+        $p['title'] = $product['name'];
         $p['link'] = $this->getLink($product);
-        $p['description'] = DfTools::cleanString($product['description_short']);
-        $p['alternate_description'] = DfTools::cleanString($product['description']);
-        $p['meta_title'] = DfTools::cleanString($product['meta_title']);
-        $p['meta_description'] = DfTools::cleanString($product['meta_description']);
+        $p['description'] = $product['description_short'];
+        $p['alternate_description'] = $product['description'];
+        $p['meta_title'] = $product['meta_title'];
+        $p['meta_description'] = $product['meta_description'];
         $p['image_link'] = $this->getImageLink($product);
         $p['images_links'] = $this->getImagesLinks($product);
         $p['main_category'] = DfTools::cleanString($product['main_category']);
